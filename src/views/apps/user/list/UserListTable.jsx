104e17fa93df8e159382c82e339537f0424780d1
fio2002 <PI76447366@idat.edu.pe>
2024-07-25 17:07:44 -0500

    Agrege filtros y paginacion en el pie de la tabla list user

--- src/views/apps/user/list/UserListTable.jsx
+++ src/views/apps/user/list/UserListTable.jsx
@@ -7,8 +7,11 @@ import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { useParams } from 'next/navigation'
 
+// Material-UI Imports
 import Grid from '@mui/material/Grid'
 import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
@@ -16,14 +19,13 @@ import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
-
-import { IconButton } from '@mui/material'
-
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import TablePagination from '@mui/material/TablePagination'
+import IconButton from '@mui/material/IconButton'
 import { toast } from 'react-toastify'
 
-import OptionMenu from '@core/components/option-menu'
-
-// Nuevas importaciones
+// Component Imports
 import AddUserDrawer from './AddUserDrawer'
 
 // Server Action Imports
@@ -33,8 +35,12 @@ import { getLocalizedUrl } from '@/utils/i18n'
 
 const UserListTable = () => {
   const [usuarios, setUsuarios] = useState([])
+  const [filteredUsuarios, setFilteredUsuarios] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [addUserOpen, setAddUserOpen] = useState(false)
+  const [selectedRole, setSelectedRole] = useState('') // Estado para el rol seleccionado
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(5)
 
   const { lang: locale } = useParams()
 
@@ -44,8 +50,8 @@ const UserListTable = () => {
 
       if (response.status === 200) {
         setUsuarios(response.data)
+        setFilteredUsuarios(response.data) // Inicialmente mostrar todos los usuarios
         setIsLoading(false)
-        console.log(response.data)
       } else {
         console.error('Error al obtener los usuarios:', response.status)
         setIsLoading(false)
@@ -60,8 +66,20 @@ const UserListTable = () => {
     obtenerUsuarios()
   }, [])
 
+  useEffect(() => {
+    if (selectedRole) {
+      setFilteredUsuarios(usuarios.filter(user => user.role?.description === selectedRole))
+    } else {
+      setFilteredUsuarios(usuarios)
+    }
+  }, [selectedRole, usuarios])
+
   const handleUserAdded = newUser => {
     setUsuarios(prevUsuarios => [...prevUsuarios, newUser])
+
+    if (selectedRole === newUser.role?.description) {
+      setFilteredUsuarios(prevUsuarios => [...prevUsuarios, newUser])
+    }
   }
 
   const deleteProduct = async id => {
@@ -75,6 +93,15 @@ const UserListTable = () => {
         toast.error('No se pudo eliminar el usuario')
       }
     }
+  }
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(parseInt(event.target.value, 10))
+    setPage(0)
   }
 
   return (
@@ -93,9 +120,6 @@ const UserListTable = () => {
           <Button
             variant='contained'
             startIcon={<i className='tabler-plus' />}
-            
-            // onClick={() => setAddUserOpen(!addUserOpen)
-
             onClick={() => {
               setAddUserOpen(true)
             }}
@@ -106,70 +130,99 @@ const UserListTable = () => {
           </Button>
         </Grid>
         <Grid item xs={12}>
-          <TableContainer component={Paper}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  {/* <TableCell>ID</TableCell> */}
-                  <TableCell>Nombre</TableCell>
-                  <TableCell>Apellido Paterno</TableCell>
-                  <TableCell>Apellido Materno</TableCell>
-                  <TableCell>Email</TableCell>
-                  <TableCell>Rol</TableCell>
-                  <TableCell>Estado</TableCell>
-                  <TableCell>Numero de Documento</TableCell>
-                  <TableCell>Pais</TableCell>
-                  <TableCell>Fecha de Creación</TableCell>
-                  <TableCell>Última Actualización</TableCell>
-                  <TableCell>Acciones</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {isLoading ? (
-                  <TableRow>
-                    <TableCell colSpan={10} align='center'>
-                      Cargando usuarios...
-                    </TableCell>
-                  </TableRow>
-                ) : usuarios.length > 0 ? (
-                  usuarios.map(usuario => (
-                    <TableRow key={usuario.id}>
-                      {/* <TableCell>{usuario.id}</TableCell> */}
-                      <TableCell>{usuario.name}</TableCell>
-                      <TableCell>{usuario.last_nameF}</TableCell>
-                      <TableCell>{usuario.last_nameS}</TableCell>
-                      <TableCell>{usuario.email}</TableCell>
-                      <TableCell>{usuario.role?.description}</TableCell>
-                      <TableCell>{usuario.is_active ? 'Activo' : 'Inactivo'}</TableCell>
-                      <TableCell>{usuario.doc_num}</TableCell>
-                      <TableCell>{usuario.country_display}</TableCell>
+          <Card>
+            <CardContent>
+              <Grid container spacing={2}>
 
-                      <TableCell>{new Date(usuario.create_at).toLocaleString()}</TableCell>
-                      <TableCell>{new Date(usuario.updated_at).toLocaleString()}</TableCell>
-                      <TableCell>
-                        <div className='flex items-center'>
-                          <IconButton onClick={() => deleteProduct(usuario.id)}>
-                            <i className='tabler-trash text-[22px] text-textSecondary' />
-                          </IconButton>
-                          <IconButton>
-                            <Link href={getLocalizedUrl(`apps/user/view/${usuario.id}`, locale)} className='flex'>
-                              <i className='tabler-eye text-[22px] text-textSecondary' />
-                            </Link>
-                          </IconButton>
-                        </div>
-                      </TableCell>
+                <Grid item xs={12} sm={12} md={4}>
+                  <Select
+                    value={selectedRole}
+                    onChange={(e) => setSelectedRole(e.target.value)}
+                    displayEmpty
+                    inputProps={{ 'aria-label': 'Select Role' }}
+                    fullWidth
+                  >
+                    <MenuItem value=''>Todos</MenuItem>
+                    <MenuItem value='País Miembro (PPMM)'>País Miembro (PPMM)</MenuItem>
+                    <MenuItem value='Administrador del sistema'>Administrador del sistema</MenuItem>
+                    <MenuItem value='Secretaria Técnica (SGCAN)'>Secretaria Técnica (SGCAN)</MenuItem>
+                  </Select>
+                </Grid>
+              </Grid>
+              <TableContainer component={Paper} sx={{ marginTop: 2 }}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Nombre</TableCell>
+                      <TableCell>Apellido Paterno</TableCell>
+                      <TableCell>Apellido Materno</TableCell>
+                      <TableCell>Email</TableCell>
+                      <TableCell>Rol</TableCell>
+                      <TableCell>Estado</TableCell>
+                      <TableCell>Numero de Documento</TableCell>
+                      <TableCell>Pais</TableCell>
+                      <TableCell>Fecha de Creación</TableCell>
+                      <TableCell>Última Actualización</TableCell>
+                      <TableCell>Acciones</TableCell>
                     </TableRow>
-                  ))
-                ) : (
-                  <TableRow>
-                    <TableCell colSpan={10} align='center'>
-                      No se encontraron usuarios.
-                    </TableCell>
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </TableContainer>
+                  </TableHead>
+                  <TableBody>
+                    {isLoading ? (
+                      <TableRow>
+                        <TableCell colSpan={11} align='center'>
+                          Cargando usuarios...
+                        </TableCell>
+                      </TableRow>
+                    ) : filteredUsuarios.length > 0 ? (
+                      filteredUsuarios
+                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        .map(usuario => (
+                          <TableRow key={usuario.id}>
+                            <TableCell>{usuario.name}</TableCell>
+                            <TableCell>{usuario.last_nameF}</TableCell>
+                            <TableCell>{usuario.last_nameS}</TableCell>
+                            <TableCell>{usuario.email}</TableCell>
+                            <TableCell>{usuario.role?.description}</TableCell>
+                            <TableCell>{usuario.is_active ? 'Activo' : 'Inactivo'}</TableCell>
+                            <TableCell>{usuario.doc_num}</TableCell>
+                            <TableCell>{usuario.country_display}</TableCell>
+                            <TableCell>{new Date(usuario.create_at).toLocaleString()}</TableCell>
+                            <TableCell>{new Date(usuario.updated_at).toLocaleString()}</TableCell>
+                            <TableCell>
+                              <div className='flex items-center'>
+                                <IconButton onClick={() => deleteProduct(usuario.id)}>
+                                  <i className='tabler-trash text-[22px] text-textSecondary' />
+                                </IconButton>
+                                <IconButton>
+                                  <Link href={getLocalizedUrl(`apps/user/view/${usuario.id}`, locale)} className='flex'>
+                                    <i className='tabler-eye text-[22px] text-textSecondary' />
+                                  </Link>
+                                </IconButton>
+                              </div>
+                            </TableCell>
+                          </TableRow>
+                        ))
+                    ) : (
+                      <TableRow>
+                        <TableCell colSpan={11} align='center'>
+                          No se encontraron usuarios.
+                        </TableCell>
+                      </TableRow>
+                    )}
+                  </TableBody>
+                </Table>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 25]}
+                  component="div"
+                  count={filteredUsuarios.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+              </TableContainer>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
 
