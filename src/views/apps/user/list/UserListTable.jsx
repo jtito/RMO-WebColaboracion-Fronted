64f5d355fafe0af5fee36bf6f05aeba80e875871
Franc02306 <francomunoz230602@gmail.com>
2024-07-27 21:58:03 -0500

    cambios select perpage

--- src/views/apps/user/list/UserListTable.jsx
+++ src/views/apps/user/list/UserListTable.jsx
@@ -20,8 +20,10 @@ import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
-
-import { IconButton } from '@mui/material'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import TablePagination from '@mui/material/TablePagination'
+import IconButton from '@mui/material/IconButton'
 
 import { toast } from 'react-toastify'
 
@@ -38,8 +40,12 @@ import { getLocalizedUrl } from '@/utils/i18n'
 
 const UserListTable = () => {
   const [usuarios, setUsuarios] = useState([])
+  const [filteredUsuarios, setFilteredUsuarios] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [addUserOpen, setAddUserOpen] = useState(false)
+  const [selectedRole, setSelectedRole] = useState('') // Estado para el rol seleccionado
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(5)
 
   const { lang: locale } = useParams()
 
@@ -49,8 +55,8 @@ const UserListTable = () => {
 
       if (response.status === 200) {
         setUsuarios(response.data)
+        setFilteredUsuarios(response.data) // Inicialmente mostrar todos los usuarios
         setIsLoading(false)
-        console.log(response.data)
       } else {
         console.error('Error al obtener los usuarios:', response.status)
         setIsLoading(false)
@@ -65,6 +71,14 @@ const UserListTable = () => {
     obtenerUsuarios()
   }, [])
 
+  useEffect(() => {
+    if (selectedRole) {
+      setFilteredUsuarios(usuarios.filter(user => user.role?.description === selectedRole))
+    } else {
+      setFilteredUsuarios(usuarios)
+    }
+  }, [selectedRole, usuarios])
+
   const handleUserAdded = async newUser => {
     try {
       const response = await obtnerUsuarios()
@@ -74,6 +88,10 @@ const UserListTable = () => {
         const addedUser = updatedUsers.find(user => user.id === newUser.id)
 
         setUsuarios(prevUsuarios => [...prevUsuarios, addedUser])
+        
+        if (selectedRole === addedUser.role?.description) {
+          setFilteredUsuarios(prevUsuarios => [...prevUsuarios, addedUser])
+        }
       }
     } catch (error) {
       console.error('Error fetching updated user data:', error)
@@ -94,6 +112,15 @@ const UserListTable = () => {
     }
   }
 
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(parseInt(event.target.value, 10))
+    setPage(0)
+  }
+
   return (
     <>
       <Grid container spacing={2}>
@@ -110,9 +137,6 @@ const UserListTable = () => {
           <Button
             variant='contained'
             startIcon={<i className='tabler-plus' />}
-            
-            // onClick={() => setAddUserOpen(!addUserOpen)
-
             onClick={() => {
               setAddUserOpen(true)
             }}
@@ -122,18 +146,30 @@ const UserListTable = () => {
             Agregar Usuario
           </Button>
         </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Select
+            value={selectedRole}
+            onChange={(e) => setSelectedRole(e.target.value)}
+            displayEmpty
+            inputProps={{ 'aria-label': 'Select Role' }}
+            fullWidth
+          >
+            <MenuItem value=''>Todos</MenuItem>
+            <MenuItem value='País Miembro (PPMM)'>País Miembro (PPMM)</MenuItem>
+            <MenuItem value='Administrador del sistema'>Administrador del sistema</MenuItem>
+            <MenuItem value='Secretaria Técnica (SGCAN)'>Secretaria Técnica (SGCAN)</MenuItem>
+          </Select>
+        </Grid>
         <Grid item xs={12}>
-          <TableContainer component={Paper}>
+          <TableContainer component={Paper} sx={{ marginTop: 2 }}>
             <Table>
               <TableHead>
                 <TableRow>
-                  {/* <TableCell>ID</TableCell> */}
                   <TableCell>Nombre</TableCell>
                   <TableCell>Apellido Paterno</TableCell>
                   <TableCell>Apellido Materno</TableCell>
                   <TableCell>Email</TableCell>
                   <TableCell>Rol</TableCell>
-                  {/* <TableCell>Estado</TableCell> */}
                   <TableCell>Numero de Documento</TableCell>
                   <TableCell>Pais</TableCell>
                   <TableCell>Fecha de Creación</TableCell>
@@ -148,38 +184,38 @@ const UserListTable = () => {
                       Cargando usuarios...
                     </TableCell>
                   </TableRow>
-                ) : usuarios.length > 0 ? (
-                  usuarios.map(usuario => (
-                    <TableRow key={usuario.id}>
-                      {/* <TableCell>{usuario.id}</TableCell> */}
-                      <TableCell>{usuario.name}</TableCell>
-                      <TableCell>{usuario.last_nameF}</TableCell>
-                      <TableCell>{usuario.last_nameS}</TableCell>
-                      <TableCell>{usuario.email}</TableCell>
-                      <TableCell>{usuario.role?.description}</TableCell>
-                      {/* <TableCell>{usuario.is_active ? 'Activo' : 'Inactivo'}</TableCell> */}
-                      <TableCell>{usuario.doc_num}</TableCell>
-                      <TableCell>{usuario.country_display}</TableCell>
-                      <TableCell>
-                        {usuario?.create_at? format(new Date(usuario?.create_at), 'dd/MM/yyyy', { locale: es }): 'Fecha no disponible'}
-                      </TableCell>
-                      <TableCell>
-                        {usuario?.updated_at? format(new Date(usuario?.updated_at), 'dd/MM/yyyy', { locale: es }): 'Fecha no disponible'}
-                      </TableCell>
-                      <TableCell>
-                        <div className='flex items-center'>
-                          <IconButton onClick={() => deleteProduct(usuario.id)}>
-                            <i className='tabler-trash text-[22px] text-textSecondary' />
-                          </IconButton>
-                          <IconButton>
-                            <Link href={getLocalizedUrl(`apps/user/view/${usuario.id}`, locale)} className='flex'>
-                              <i className='tabler-eye text-[22px] text-textSecondary' />
-                            </Link>
-                          </IconButton>
-                        </div>
-                      </TableCell>
-                    </TableRow>
-                  ))
+                ) : filteredUsuarios.length > 0 ? (
+                  filteredUsuarios
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map(usuario => (
+                      <TableRow key={usuario.id}>
+                        <TableCell>{usuario.name}</TableCell>
+                        <TableCell>{usuario.last_nameF}</TableCell>
+                        <TableCell>{usuario.last_nameS}</TableCell>
+                        <TableCell>{usuario.email}</TableCell>
+                        <TableCell>{usuario.role?.description}</TableCell>
+                        <TableCell>{usuario.doc_num}</TableCell>
+                        <TableCell>{usuario.country_display}</TableCell>
+                        <TableCell>
+                          {usuario?.create_at? format(new Date(usuario?.create_at), 'dd/MM/yyyy', { locale: es }): 'Fecha no disponible'}
+                        </TableCell>
+                        <TableCell>
+                          {usuario?.updated_at? format(new Date(usuario?.updated_at), 'dd/MM/yyyy', { locale: es }): 'Fecha no disponible'}
+                        </TableCell>
+                        <TableCell>
+                          <div className='flex items-center'>
+                            <IconButton onClick={() => deleteProduct(usuario.id)}>
+                              <i className='tabler-trash text-[22px] text-textSecondary' />
+                            </IconButton>
+                            <IconButton>
+                              <Link href={getLocalizedUrl(`apps/user/view/${usuario.id}`, locale)} className='flex'>
+                                <i className='tabler-eye text-[22px] text-textSecondary' />
+                              </Link>
+                            </IconButton>
+                          </div>
+                        </TableCell>
+                      </TableRow>
+                    ))
                 ) : (
                   <TableRow>
                     <TableCell colSpan={10} align='center'>
@@ -189,6 +225,15 @@ const UserListTable = () => {
                 )}
               </TableBody>
             </Table>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              component="div"
+              count={filteredUsuarios.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
           </TableContainer>
         </Grid>
       </Grid>
