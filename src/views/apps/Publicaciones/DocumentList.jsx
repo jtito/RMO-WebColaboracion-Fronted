39dc1dc279140a9ee779c245646b893cf2449714
Franc02306 <francomunoz230602@gmail.com>
2024-07-30 04:00:56 -0500

    diseño de borrador

--- src/views/apps/Publicaciones/DocumentList.jsx
+++ src/views/apps/Publicaciones/DocumentList.jsx
@@ -1,120 +1,134 @@
-"use client";
+'use client'
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation'
+
+import IconButton from '@mui/material/IconButton'
 
 import {
-  Button, Card, CardActions, CardContent, CardMedia, Divider, FormControl,
-  Grid, InputLabel, MenuItem, Select, TextField, Typography, Dialog
-} from '@mui/material';
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Divider,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+  Dialog
+} from '@mui/material'
 
-import AddDoc from './AddDoc';
-import SelectUsers from './SelectUsers';
-import { obtenerDocumentos, obtenerperfil } from '@/Service/axios.services';
+import AddDoc from './AddDoc'
+import SelectUsers from './SelectUsers'
+import { obtenerDocumentos, obtenerperfil } from '@/Service/axios.services'
 
 const DocumentList = ({ type }) => {
-  const [openAddDoc, setOpenAddDoc] = useState(false);
-  const [openSelectUsers, setOpenSelectUsers] = useState(false);
-  const [coverImage, setCoverImage] = useState(null);
-  const [openImageDialog, setOpenImageDialog] = useState(false);
-  const [perfil, setperfil] = useState([]);
-  const [docperf, setdocperf] = useState([]);
-  const [filteredDocs, setFilteredDocs] = useState([]);
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
+  const [openAddDoc, setOpenAddDoc] = useState(false)
+  const [openSelectUsers, setOpenSelectUsers] = useState(false)
+  const [coverImage, setCoverImage] = useState(null)
+  const [openImageDialog, setOpenImageDialog] = useState(false)
+  const [perfil, setperfil] = useState([])
+  const [docperf, setdocperf] = useState([])
+  const [filteredDocs, setFilteredDocs] = useState([])
+  const [startDate, setStartDate] = useState('')
+  const [endDate, setEndDate] = useState('')
 
-  const router = useRouter();
+  const router = useRouter()
 
   const obtenerperfildoc = async () => {
     try {
-      const response = await obtenerperfil();
+      const response = await obtenerperfil()
 
       if (response.status === 200) {
-        setperfil(response.data);
+        setperfil(response.data)
       } else {
-        console.error('Error al obtener los perfiles:', response.status);
+        console.error('Error al obtener los perfiles:', response.status)
       }
     } catch (error) {
-      console.error('Error en la solicitud:', error);
+      console.error('Error en la solicitud:', error)
     }
-  };
+  }
 
   const obtenerDocumentosperf = async () => {
     try {
-      const response = await obtenerDocumentos();
-      
+      const response = await obtenerDocumentos()
+
       if (response.status === 200) {
-        setdocperf(response.data);
+        setdocperf(response.data)
       } else {
-        console.error('Error al obtener los documentos:', response.status);
+        console.error('Error al obtener los documentos:', response.status)
       }
     } catch (error) {
-      console.error('Error en la solicitud:', error);
+      console.error('Error en la solicitud:', error)
     }
-  };
+  }
 
   useEffect(() => {
-    obtenerperfildoc();
-    obtenerDocumentosperf();
-  }, [type]);
+    obtenerperfildoc()
+    obtenerDocumentosperf()
+  }, [type])
 
   useEffect(() => {
     const filterDocs = () => {
-      let filtered = [];
-      
-      if (type === "publicados") {
-        filtered = docperf.filter(doc => doc.state.description === "Publicado");
-      } else if (type === "borradores") {
-        filtered = docperf.filter(doc => doc.state.description === "Borrador");
+      let filtered = []
+
+      if (type === 'publicados') {
+        filtered = docperf.filter(doc => doc.state.description === 'Publicado')
+      } else if (type === 'borradores') {
+        filtered = docperf.filter(doc => doc.state.description === 'Borrador')
       }
 
       if (startDate) {
-        filtered = filtered.filter(doc => new Date(doc.updated_at) >= new Date(startDate));
+        filtered = filtered.filter(doc => new Date(doc.updated_at) >= new Date(startDate))
       }
 
       if (endDate) {
-        filtered = filtered.filter(doc => new Date(doc.updated_at) <= new Date(endDate));
+        filtered = filtered.filter(doc => new Date(doc.updated_at) <= new Date(endDate))
       }
 
-      setFilteredDocs(filtered);
-    };
+      setFilteredDocs(filtered)
+    }
 
-    filterDocs();
-  }, [docperf, type, startDate, endDate]);
+    filterDocs()
+  }, [docperf, type, startDate, endDate])
 
   const handleClickOpenAddDoc = () => {
-    setOpenAddDoc(true);
-  };
+    setOpenAddDoc(true)
+  }
 
   const handleCloseAddDoc = () => {
-    setOpenAddDoc(false);
-  };
+    setOpenAddDoc(false)
+  }
 
   const handleNext = docData => {
-    setCoverImage(docData.file);
-    setOpenSelectUsers(true);
-  };
+    setCoverImage(docData.file)
+    setOpenSelectUsers(true)
+  }
 
   const handleCloseSelectUsers = () => {
-    setOpenSelectUsers(false);
-  };
+    setOpenSelectUsers(false)
+  }
 
   const handleConfirm = userData => {
     // Maneja los datos de usuarios, roles y permisos aquí
-  };
+  }
 
   const handleImageClick = () => {
-    setOpenImageDialog(true);
-  };
+    setOpenImageDialog(true)
+  }
 
   const handleCloseImageDialog = () => {
-    setOpenImageDialog(false);
-  };
+    setOpenImageDialog(false)
+  }
 
   const handleCardClick = id => {
-    router.push(`${type}/${id}`);
-  };
+    router.push(`${type}/${id}`)
+  }
 
   return (
     <Grid container spacing={6}>
@@ -122,12 +136,7 @@ const DocumentList = ({ type }) => {
         <Grid container spacing={8} alignItems='center'>
           <Grid item xs={12} md={8} container spacing={12}>
             <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                variant='outlined'
-                label='Buscar'
-                placeholder='Buscar...'
-              />
+              <TextField fullWidth variant='outlined' label='Buscar' placeholder='Buscar...' />
             </Grid>
             <Grid item xs={12} sm={3}>
               <TextField
@@ -158,11 +167,7 @@ const DocumentList = ({ type }) => {
 
         {filteredDocs.length > 0 ? (
           filteredDocs.map(documento => (
-            <Card
-              key={documento.id}
-              sx={{ mb: 2, cursor: 'pointer' }}
-              onClick={() => handleCardClick(documento.id)}
-            >
+            <Card key={documento.id} sx={{ mb: 2, cursor: 'pointer' }} onClick={() => handleCardClick(documento.id)}>
               <CardContent>
                 <Grid container spacing={2}>
                   <Grid item xs={12} sm={2}>
@@ -174,25 +179,56 @@ const DocumentList = ({ type }) => {
                       onClick={handleImageClick}
                     />
                   </Grid>
-                  <Grid item xs={12} sm={8}>
-                    <Typography variant='h5' component='div'>
-                      {documento.title}
+                  <Grid item xs={12} sm={10}>
+                    <Grid container alignItems='center'>
+                      <Grid item xs>
+                        <Typography variant='h4' component='div'>
+                          {documento.title}
+                        </Typography>
+                      </Grid>
+                      <Grid item>
+                        <IconButton>
+                          <i className='tabler-trash text-[26px] text-textSecondary' />
+                        </IconButton>
+                      </Grid>
+                    </Grid>
+                    <Typography 
+                      variant='body2' 
+                      color='text.secondary'
+                      sx={{ maxWidth: '35vw'}}>
+                        {documento.description}
                     </Typography>
-                    <Typography variant='body2' color='text.secondary'>
-                      {documento.description}
-                    </Typography>
-                    <Typography variant='body2' color='text.secondary'>
-                      Última modificación: {new Date(documento.updated_at).toLocaleDateString()}
-                    </Typography>
-                    <Typography variant='body2' color='text.secondary'>
+                    <Grid container spacing={2}>
+                      <Grid item xs={12} sm={6}>
+                        <Typography variant='body2' color='text.secondary'>
+                          Fecha de creación: {new Date(documento.create_at).toLocaleDateString()}
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <Typography variant='body2' color='text.secondary' textAlign='right'>
+                          Última modificación: {new Date(documento.updated_at).toLocaleDateString()}
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                    {/*<Typography variant='body2' color='text.secondary'>
                       Estado: {documento.state.description}
-                    </Typography>
+                    </Typography>*/}
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                      {documento.state.description === 'Borrador' && (
+                        <Button variant='contained' color='primary' startIcon={<i className='bi bi-send' />}>
+                          Borrador
+                          <IconButton>
+                            <i className='tabler-send text-[22px] text-textSecondary' />
+                          </IconButton>
+                        </Button>
+                      )}
+                    </Grid>
                   </Grid>
                 </Grid>
               </CardContent>
-              <CardActions className='card-actions-dense'>
+              {/*<CardActions className='card-actions-dense'>
                 <Button>Acción</Button>
-              </CardActions>
+              </CardActions>*/}
             </Card>
           ))
         ) : (
@@ -218,7 +254,7 @@ const DocumentList = ({ type }) => {
         </Dialog>
       </Grid>
     </Grid>
-  );
-};
+  )
+}
 
-export default DocumentList;
+export default DocumentList
