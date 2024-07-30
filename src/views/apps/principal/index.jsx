874a9c3366d3b37b6e874970d438aa57248bde24
fio2002 <Pi76447366@idat.edu.pe>
2024-07-29 20:50:03 -0500

    se agrego un editor de texto como prueba

--- src/views/apps/principal/index.jsx
+++ src/views/apps/principal/index.jsx
@@ -6,37 +6,13 @@ import LinkIcon from '@mui/icons-material/Link';
 
 import LikeButton from './LikeButton'
 
+import Documentos from './Documentos'
 
 const Principal = () => {
   return (
     <Grid container spacing={6}>
       <Grid item xs={12}>
-        <Card>
-          <CardContent>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={2}>
-                <CardMedia
-                  component="img"
-                  image="https://via.placeholder.com/150" // Reemplaza con la URL de tu imagen
-                  alt="Descripción de la imagen"
-                  style={{ width: '50%', height: 'auto' }}
-                />
-              </Grid>
-              <Grid item xs={12} sm={8}>
-                <Typography variant="h5" component="div">
-                  Título del Contenido
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Esta es una breve descripción del contenido. Puedes agregar aquí más detalles para complementar la imagen.
-                </Typography>
-              </Grid>
-            </Grid>
-          </CardContent>
-          <CardActions className='card-actions-dense'>
-            <LikeButton count={123} />
-
-          </CardActions>
-        </Card>
+ <Documentos></Documentos>
       </Grid>
 
     </Grid>
