88b33a679d25bd2a5cc218e499a8e6529a12ee31
fio2002 <Pi76447366@idat.edu.pe>
2024-08-02 17:26:29 -0500

    se arreglo borradores

---
+++ src/views/apps/Publicaciones/Borradores/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import DocumentList from '../DocumentList';
+
+const Borradores = () => {
+  return <DocumentList type="borradores" />;
+};
+
+
+export default Borradores
